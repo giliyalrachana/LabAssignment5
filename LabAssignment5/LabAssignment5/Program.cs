ab6f62b3d33df0506336013e8443b584449e5fc1
Rachana <giliyalrachana@gmail.com>
2021-09-23 21:46:50 +0530

    LabAssignment5

---
+++ LabAssignment5/LabAssignment5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace LabAssignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.Write("Enter any number : ");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+                fact = fact * i;
+
+            Console.Write("Factorial of" +number+ "is:" +fact);
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
---
+++ LabAssignment5/LabAssignment5/SimpleInterest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LabAssignment5
+{
+    class SimpleInterest
+    {
+        static void Main()
+        {
+            int P, T;
+            float R, SI;
+            Console.Write("Enter the Amount:");
+            P = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Rate:");
+            R = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Time:");
+            T = Convert.ToInt32(Console.ReadLine());
+            SI = P * T * R / 100;
+            Console.WriteLine("Simple Interest is : {0}", SI);
+            Console.ReadLine();
+        }
+    }
+}
